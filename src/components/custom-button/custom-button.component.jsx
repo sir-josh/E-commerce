78ebd66ea695572edf2ceb216eb1de9d65d8b693
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2020-08-07 11:14:57 +0100

    Fixed the Custom button component to have the type of submit

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './custom-button.styles.scss';
 
 const CustomButton = ({ children, isGoogleSignIn, ...otherProps}) => (
-    <button className={`${isGoogleSignIn ? 'google-sign-in': ''} custom-button`} {...otherProps}>
+    <button type="button" className={`${isGoogleSignIn ? 'google-sign-in': ''} custom-button`} {...otherProps}>
         { children }
     </button>
 );
