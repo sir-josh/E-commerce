eac6be340dcd35b9dead6ef05c6d9615914032df
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2020-08-11 06:21:02 +0100

    Added checkout-item component with it's styles on the checkout page

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
 import { selectCartItems, selectCartTotalAmount } from "../../redux/cart/cart.selectors";
+import CheckoutItem from "../../components/checkout-item/checkout-item.component";
 
 import './checkout.styles.scss';
 
@@ -26,7 +27,9 @@ const CheckoutPage = ({cartItems, total}) => (
             </div>
         </div>
         {
-            cartItems.map(cartItem => cartItem.name)
+            cartItems.map(cartItem => (
+                <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+            ))
         }
         <div className="total">
             <span>TOTAL: ${total}</span>
