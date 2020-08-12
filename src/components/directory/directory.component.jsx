9e167d7ac236afe7a648b78d2f544f18e1148652
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2020-08-12 02:30:11 +0100

    Implemented storing cart items to the localStorage using redux-persist library, created directory reducer for directory component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,61 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector} from 'reselect';
+import { selectDirectorySections } from '../../redux/directory/directory.selectors';
+
 import MenuItem from '../menu-item/menu-item.component';
 
 import './directory.styles.scss';
 
-class directory extends Component {
-    constructor(){
-        super();
-
-        this.state = {
-            sections: [
-                {
-                    title: 'hats',
-                    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-                    id: 1,
-                    linkUrl: 'hats'
-                  },
-                  {
-                    title: 'jackets',
-                    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-                    id: 2,
-                    linkUrl: ''
-                  },
-                  {
-                    title: 'sneakers',
-                    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-                    id: 3,
-                    linkUrl: ''
-                  },
-                  {
-                    title: 'womens',
-                    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-                    size: 'large',
-                    id: 4,
-                    linkUrl: ''
-                  },
-                  {
-                    title: 'mens',
-                    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-                    size: 'large',
-                    id: 5,
-                    linkUrl: ''
-                  }
-            ]
+const directory = ({ sections }) => (
+    <div className="directory-menu">
+        {
+            sections.map(({ id, imageUrl, ...otherSectionProps }) => (
+                <MenuItem key={id} imageLink={imageUrl} {...otherSectionProps} />
+            ))
         }
-    }
+    </div>
+)
 
-    render() {
-        return (
-            <div className="directory-menu">
-                {
-                    this.state.sections.map(({ id, imageUrl, ...otherSectionProps }) => (
-                        <MenuItem key={id} imageLink={imageUrl} {...otherSectionProps} />
-                    ))
-                }
-            </div>
-        )
-    }
-}
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections
+});
 
-export default directory;
+export default connect(mapStateToProps)(directory);
