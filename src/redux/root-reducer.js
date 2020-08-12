dcc5e4e049c1e17ed53c5ec0c55d6ed64de39474
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2020-08-12 02:30:11 +0100

    Implemented storing cart items to the localStorage using redux-persist library, also created directory reducer for the directory component

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,21 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";  //This gets the computer localStorage
 
 import  userReducer  from "./user/user.reducer";
 import cartReducer from './cart/cart.reducer';
+import directoryReducer from './directory/directory.reducer';
 
-export default combineReducers({
+const persistConfig = {
+    key: 'root',
+    storage,
+    whiteList: ['cart']  //This lets the redux-persist know to only persist the Reducers in the array to the localStorage
+}
+
+const rootReducer = combineReducers({
     user: userReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    directory: directoryReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,13 @@
 import { createStore, applyMiddleware } from "redux";
+import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+ const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+ const persistor = persistStore(store);
+
+export { store, persistor };
