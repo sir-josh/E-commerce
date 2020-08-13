c36f343c283db380cfed0b470c48afe3c55fed28
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2020-08-13 18:54:03 +0100

    Created Collection component nexted route, created Collection_ID_Map object to map params value to their respective ID value

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,7 +3,7 @@ import { withRouter } from 'react-router-dom';
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageLink, size,history, linkUrl, match }) => (
+const MenuItem = ({ title, imageLink, size, history, linkUrl, match }) => (
     <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`) }>
         <div className="background-image" style={{ backgroundImage: `url(${imageLink})`}} />
         <div className="content">
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,8 +1,23 @@
 import { createSelector } from "reselect";
 
+const COLLECTION_ID_MAP = {
+    hats: 1,
+    sneakers: 2,
+    jackets: 3,
+    womens: 4,
+    mens: 5
+}
+
 const selectCollections = state => state.shop;
 
 export const selectCollectionsItems = createSelector(
     [selectCollections],
     shop => shop.collections
+);
+
+// NB: collectionUrlID(A string paramter value gotten from the url) is in string format, we want to use "COLLECTION_ID_MAP" obj to convert 
+// the string value to an equivalent collection id (that is from the collection list) which is in interger format
+export const selectACollectionFromCollections = collectionUrlID => createSelector(
+    [selectCollectionsItems],
+    collections => collections.find(collection => collection.id === COLLECTION_ID_MAP[collectionUrlID])
 );
