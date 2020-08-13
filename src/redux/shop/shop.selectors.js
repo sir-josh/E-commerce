d29030f468f84b315cec4e81e6f5ea70caa59da0
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2020-08-13 19:04:57 +0100

    shop data normalization from 'array of obj' to 'obj of obj'

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,13 +1,5 @@
 import { createSelector } from "reselect";
 
-const COLLECTION_ID_MAP = {
-    hats: 1,
-    sneakers: 2,
-    jackets: 3,
-    womens: 4,
-    mens: 5
-}
-
 const selectCollections = state => state.shop;
 
 export const selectCollectionsItems = createSelector(
@@ -15,9 +7,7 @@ export const selectCollectionsItems = createSelector(
     shop => shop.collections
 );
 
-// NB: collectionUrlID(A string paramter value gotten from the url) is in string format, we want to use "COLLECTION_ID_MAP" obj to convert 
-// the string value to an equivalent collection id (that is from the collection list) which is in interger format
 export const selectACollectionFromCollections = collectionUrlID => createSelector(
     [selectCollectionsItems],
-    collections => collections.find(collection => collection.id === COLLECTION_ID_MAP[collectionUrlID])
+    collections => collections[collectionUrlID]
 );
