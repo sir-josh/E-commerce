f5b6f9f6e6500455eb021d2667f850e85f2fc2a1
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2020-08-24 23:34:36 +0100

    Incorporate saga middleware into store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { createStore, applyMiddleware } from "redux";
 import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
-import thunk from 'redux-thunk';
+import createSagaMiddleware from "redux-saga";
+
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [sagaMiddleware];
 
 import rootReducer from './root-reducer';
-
-const middlewares = [thunk];
 
 if(process.env.NODE_ENV === 'development'){
     middlewares.push(logger);
