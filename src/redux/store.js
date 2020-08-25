19c41ef0d660e4d3774281f79bcf8e0db17f3f4b
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2020-08-25 04:55:54 +0100

    Create a 'root-saga.js' file that runs all sagas in the application concurrently

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,8 @@ import { createStore, applyMiddleware } from "redux";
 import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 import createSagaMiddleware from "redux-saga";
-import { fetchCollectionsStart } from './shop/shop.sagas';
+
+import rootSaga from './root-saga';
 import rootReducer from './root-reducer';
 
 const sagaMiddleware = createSagaMiddleware();
@@ -16,7 +17,7 @@ if(process.env.NODE_ENV === 'development'){
 
  const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
- sagaMiddleware.run(fetchCollectionsStart);
+ sagaMiddleware.run(rootSaga);
 
  const persistor = persistStore(store);
 
