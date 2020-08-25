af5289ecb10f9b71ff20a509355425843e243b0e
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2020-08-25 04:41:32 +0100

    Change 'takeEvery' to 'takeLatest' in the shop redux-saga

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,4 +1,4 @@
-import { takeEvery, call, put } from "redux-saga/effects";
+import { takeLatest, call, put } from "redux-saga/effects";
 import { firestore, convertCollectionsSnapshotToMap } from "../../firebase/firebase.utils";
 import {
     fetchCollectionsSuccess,
@@ -19,5 +19,5 @@ export function* fetchCollectionsAsync(){
 }
 
 export function* fetchCollectionsStart(){
-    yield takeEvery(ShopActionTypes.FETCH_COLLECTIONS_START, fetchCollectionsAsync);
+    yield takeLatest(ShopActionTypes.FETCH_COLLECTIONS_START, fetchCollectionsAsync);
 }
