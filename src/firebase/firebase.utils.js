b53d036161b7704c4248286ea3ad0103264ed451
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2020-08-28 03:22:29 +0100

    Convert the App component to use useEffect hooks

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -24,12 +24,6 @@ export const createUserProfileDocument = async(userAuth, additionalData) => {
     if(!snapShot.exists){
         const { displayName, email } = userAuth;
         const createdAt = new Date();
-        console.log({
-            displayName,
-            email,
-            createdAt,
-            ...additionalData
-        });
         try {
             await userRef.set({
                 displayName,
