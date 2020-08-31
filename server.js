66055356af1f90ef228a9d4c5fab0719e01dd253
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2020-09-01 00:20:42 +0100

    Set up backend server that serves the client build folder

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ if(process.env.NODE_ENV !== 'production') require('dotenv').config();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
